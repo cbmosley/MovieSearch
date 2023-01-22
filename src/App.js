c151d4f7d7b5ebc4e87fc59aff726974111dea63
cbmosley <cbmosley@ncsu.edu>
2023-01-22 12:53:11 -0500

    Created base layout of website.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from './components/SearchBar'
+import { Button } from '@mui/material/';
+import { TextField } from '@mui/material';
+import { useState } from 'react'
+import axios from 'axios';
+
+export const SearchMovie = ({movies}) => {
+  const [searchText, setSearchText] = useState('')
+  const handleSearchMovies = (e) => {
+    e.preventDefault();
+
+    axios
+      .get('')
+  }
+}
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainPage">
+    <h1>FindMyMovie</h1>
+      <div className="search">
+      <h2>Search Movies: </h2>
+        <div className="searchBar">
+        <TextField id="outlined-basic" label="Lets Find That Movie!" variant="outlined" />
+        <Button variant="contained">Search Movies</Button>
+        </div>
+      </div>
+      <div className="searchResults">
+        <h3>Results: </h3>
+      </div>
+      
     </div>
   );
 }
